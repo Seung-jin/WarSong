5df760c275b5e7b4e694603a1554b17b348406e5
LSJ <fewfree12@naver.com>
2016-09-24 22:51:08 +0900

    기본적인 플레이어 UI 간단하게 구현

---
+++ WarSong/Assets/Script/Player/PlayerScript.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+    private int castleHP;
+
+}
---
+++ WarSong/Assets/Script/Unit/UnitScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitScript : MonoBehaviour {
+    private int hp;
+    private int arrange;
+    private int damage;
+
+    public virtual void Attack()
+    {
+        print("Attack!!");
+    }
+}
